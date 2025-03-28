92236e6c527af95e5e1bfbd0ecf5e1e63a81d55f
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-03-28 23:57:02 +0530

    Added Cores Middleware

---
+++ TenantVoltAPI/cors_middleware.py
@@ -0,0 +1,10 @@
+class CorsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response["Access-Control-Allow-Origin"] = "*"  # Or specify your frontend domain
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        return response
