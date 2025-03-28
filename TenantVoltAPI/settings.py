6d7f064fee34c0d965dc9d73fbb2d97ba7c7bfc1
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-03-28 23:55:48 +0530

    Added Middleware to settings

--- TenantVoltAPI/settings.py
+++ TenantVoltAPI/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'TenantVolt.cors_middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'TenantVoltAPI.urls'
