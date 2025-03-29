bef732b0ef4badb798b7117c53765ef4d255d25a
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-03-29 22:58:26 +0530

    Updated Cors

--- TenantVoltAPI/cors_middleware.py
+++ TenantVoltAPI/cors_middleware.py
@@ -1,10 +1,20 @@
+from django.http import HttpResponse
+
 class CorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        if request.method == "OPTIONS":
+            response = HttpResponse()
+            response["Access-Control-Allow-Origin"] = "*"  # For production, use your frontend domain
+            response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+            response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+            response["Access-Control-Max-Age"] = "86400"  # 24 hours
+            return response
+
         response = self.get_response(request)
-        response["Access-Control-Allow-Origin"] = "*"  # Or specify your frontend domain
+        response["Access-Control-Allow-Origin"] = "*"
         response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
         response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
         return response
